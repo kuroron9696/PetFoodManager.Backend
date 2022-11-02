b47289faf2f8f72b1da1fb62e88e3efcbd2eaa07
kuro <40154200+kuroron9696@users.noreply.github.com>
2022-11-03 02:47:33 +0900

    🎉 プロジェクト開始

---
+++ PetFoodManager.Backend.Api/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore;
+
+namespace PetFoodManager.Backend.Api
+{
+    /// <summary>
+    /// メインプログラム
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// メイン
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// ホストビルダー生成
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+          WebHost.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingContext, config) =>
+          {
+              config.SetBasePath(Directory.GetCurrentDirectory());
+              config.AddJsonFile(Path.GetFullPath(Path.Combine(@"../appsettings.json")));
+              config.AddEnvironmentVariables();
+
+              if (hostingContext.HostingEnvironment.IsDevelopment())
+              {
+                  config.AddUserSecrets<Program>();
+              }
+          }).UseStartup<Startup>();
+    }
+}
