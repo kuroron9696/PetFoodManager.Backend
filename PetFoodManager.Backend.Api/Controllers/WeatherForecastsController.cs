544ef09ca2c4bd0b503935622dff8c8063ba8af2
kuro <40154200+kuroron9696@users.noreply.github.com>
2022-11-03 02:50:33 +0900

    🎉 プロジェクト開始 (#1)
    * 🎉 プロジェクト開始
    
    * 📝 プルリクのテンプレ追加

---
+++ PetFoodManager.Backend.Api/Controllers/WeatherForecastsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetFoodManager.Backend.Api.Domains.Dtos;
+
+namespace PetFoodManager.Backend.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastsController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastsController> _logger;
+
+    public WeatherForecastsController(ILogger<WeatherForecastsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
+
---
+++ PetFoodManager.Backend.Api/Startup.cs
@@ -0,0 +1,108 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
+namespace PetFoodManager.Backend.Api
+{
+    /// <summary>
+    /// スタートアップ
+    /// </summary>
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// コンテナにサービスを追加する
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient();
+            services.AddMvc();
+            services.AddControllersWithViews();
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "PetFoodManager.Backend.Api",
+                    Version = "v1"
+                });
+
+                s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\"",
+                });
+
+                s.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                                        {
+                                            Type = ReferenceType.SecurityScheme, Id = "Bearer"
+                                        },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+
+                s.DescribeAllParametersInCamelCase();
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                s.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddHttpContextAccessor();
+        }
+
+        /// <summary>
+        /// Http Request Pipeline
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+                app.UseSwagger();
+                app.UseSwaggerUI(s =>
+                {
+                    s.SwaggerEndpoint("/swagger/v1/swagger.json", "PetFoodManager.Backend.Api v1");
+                });
+            }
+            else
+            {
+                app.UseCors(b => b.AllowAnyOrigin().AllowAnyMethod());
+                app.UseHsts();
+            }
+
+            app.UseRouting();
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseStaticFiles();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
