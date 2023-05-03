c5c30240d1a7fefd675982263dd0d77c187581a9
kuro <40154200+kuroron9696@users.noreply.github.com>
2023-05-04 00:25:57 +0900

    ✨ レビューしてもらう処理追加

--- tools/PullRequestReviewer/Program.cs
+++ tools/PullRequestReviewer/Program.cs
@@ -1,20 +1,40 @@
 ﻿using System.Text.RegularExpressions;
 using LibGit2Sharp;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.AI.ChatCompletion;
+using static Microsoft.SemanticKernel.AI.ChatCompletion.ChatHistory;
 
 public static class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         if (args.Length != 2)
             throw new ArgumentException("引数が無効です");
 
         Console.WriteLine($"HEAD branch name: {args[0]}");
         Console.WriteLine($"Base branch name: {args[1]}");
+
+        var review = await CreateReviewAsync(args[0], args[1]);
+        Console.WriteLine(review);
+    }
+
+    private static async Task<string> CreateReviewAsync(string headBranchName, string baseBranchName)
+    {
         var repoPath = Repository.Discover(Path.GetFullPath(Directory.GetCurrentDirectory()));
+
+        var kernel = new KernelBuilder().Configure(c =>
+        {
+            c.AddOpenAIChatCompletionService("chat", "gpt-3.5-turbo", Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+        }).Build();
+
+        var chatCompletionService = kernel.GetService<IChatCompletion>();
+
+        var systemPrompt = "Provide some feedback and suggestions for improvement to source codes from user. Answer in Japanese.";
+
         using (var repo = new Repository(repoPath))
         {
-            var latestCommit = repo.Branches[args[0]].Tip;
-            var baseCommit = repo.Branches[args[1]].Tip;
+            var latestCommit = repo.Branches[headBranchName].Tip;
+            var baseCommit = repo.Branches[baseBranchName].Tip;
 
             var diff = repo.Diff.Compare<TreeChanges>(baseCommit.Tree, latestCommit.Tree);
 
@@ -25,17 +45,21 @@ public static class Program
             var classPattern = @"\bclass\s+(\w+)";
             var interfacePattern = @"\binterface\s+(\w+)";
 
+            var responses = new List<string>();
             foreach (var file in addedCSharpFiles)
             {
                 var content = File.ReadAllText(file);
                 if (Regex.IsMatch(content, classPattern) || Regex.IsMatch(content, interfacePattern))
                 {
-                    Console.WriteLine(file);
-                    Console.WriteLine("---");
-                    Console.WriteLine(content);
-                    Console.WriteLine("###");
+                    var chat = chatCompletionService.CreateNewChat();
+                    chat.AddMessage(AuthorRoles.System, systemPrompt);
+                    chat.AddMessage(AuthorRoles.User, content);
+                    var response = await chatCompletionService.GenerateMessageAsync(chat, new ChatRequestSettings { });
+                    responses.Add(response);
                 }
             }
+
+            return string.Join("\n", responses);
         }
     }
 }
