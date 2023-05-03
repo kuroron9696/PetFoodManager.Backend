d03040e695db1673ecff7a801e56519382ce079f
kuro <40154200+kuroron9696@users.noreply.github.com>
2023-05-03 21:59:36 +0900

    ⚡️ 引数でブランチ名取ってくるように

--- tools/PullRequestReviewer/Program.cs
+++ tools/PullRequestReviewer/Program.cs
@@ -1,14 +1,17 @@
 ﻿using System.Text.RegularExpressions;
 using LibGit2Sharp;
 
-public class Program
+public static class Program
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
+        if (args.Length != 1)
+            throw new ArgumentException("引数が無効です");
+
         var repoPath = Repository.Discover(Path.GetFullPath(Directory.GetCurrentDirectory()));
         using (var repo = new Repository(repoPath))
         {
-            var baseCommit = repo.Branches["origin/develop"].Tip;
+            var baseCommit = repo.Branches[args[0]].Tip;
             var latestCommit = repo.Head.Tip;
 
             var diff = repo.Diff.Compare<TreeChanges>(baseCommit.Tree, latestCommit.Tree);
@@ -17,33 +20,18 @@ public class Program
 
             var addedCSharpFiles = addedFiles.Where(e => e.EndsWith(".cs")).ToList();
 
-            var addedClasses = new List<string>();
-
-            var addedInterfaces = new List<string>();
-
-            var classPattern = new Regex(@"\bclass\s+(\w+)");
-            var interfacePattern = new Regex(@"\binterface\s+(\w+)");
+            var classPattern = @"\bclass\s+(\w+)";
+            var interfacePattern = @"\binterface\s+(\w+)";
 
             foreach (var file in addedCSharpFiles)
             {
                 var content = File.ReadAllText(file);
-                Console.WriteLine(content);
-                addedClasses.AddRange(classPattern.Matches(content).Select(e => e.Groups[1].Value));
-                addedInterfaces.AddRange(interfacePattern.Matches(content).Select(e => e.Groups[1].Value));
-            }
-
-            Console.WriteLine("Added classes:");
-
-            foreach (var addedClass in addedClasses)
-            {
-                Console.WriteLine($"- {addedClass}");
-            }
-
-            Console.WriteLine("Added interfaces:");
-
-            foreach (var addedInterface in addedInterfaces)
-            {
-                Console.WriteLine($"- {addedInterface}");
+                if (Regex.IsMatch(content, classPattern) || Regex.IsMatch(content, interfacePattern))
+                {
+                    Console.WriteLine(file);
+                    Console.WriteLine(content);
+                    Console.WriteLine("---");
+                }
             }
         }
     }
