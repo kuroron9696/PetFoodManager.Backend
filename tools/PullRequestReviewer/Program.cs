c9923048513e8a5d0ee1030dfc69ad666da013b8
kuro <40154200+kuroron9696@users.noreply.github.com>
2023-05-04 01:08:27 +0900

    ✨

--- tools/PullRequestReviewer/Program.cs
+++ tools/PullRequestReviewer/Program.cs
@@ -16,6 +16,7 @@ public static class Program
 
         var review = await CreateReviewAsync(args[0], args[1]);
         Console.WriteLine($"::set-output name=reviewComment::{review}");
+        File.WriteAllText("review.env", $"REVIEW_COMMENT={review}");
     }
 
     private static async Task<string> CreateReviewAsync(string headBranchName, string baseBranchName)
