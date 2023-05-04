e2a7e0ac92fe0a16c7a588f787045ebf34f9e004
kuro <40154200+kuroron9696@users.noreply.github.com>
2023-05-05 01:23:10 +0900

    ✨ Azure Pipelines用のyamlファイル作成(動作確認はまだ) (#10)
    * ⚡️ 出力のフォーマット調整
    
    * 🤡
    
    * ⚡️ プロンプト周りの改善
    
    * ✨ Azure Pipelines用の処理追加
    
    * 🐛
    
    * 🔊
    
    * ⚡️ いっぱい改善
    
    * 🐛
    
    * 🐛
    
    * 🐛
    
    * 🐛
    
    * 🐛
    
    * 🐛
    
    * 🐛
    
    * ⚡️ いろいろ直し
    
    * ⚡️

---
+++ tools/CommentPoster/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Octokit;
+
+/// <summary>
+/// GitHubのプルリクエストにコメントを投稿するツール
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// メイン処理
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static async Task Main(string[] args)
+    {
+        if (args.Length != 4)
+        {
+            throw new ArgumentException("引数が無効です。4つの引数が必要です: filePath, productName, repositoryId, prId");
+        }
+
+        var filePath = args[0];
+        var productName = args[1];
+        var repositoryId = long.Parse(args[2]);
+        var prId = int.Parse(args[3]);
+
+        var githubToken = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+        var githubClient = new GitHubClient(new ProductHeaderValue(productName))
+        {
+            Credentials = new Credentials(githubToken)
+        };
+
+        var json = File.ReadAllText(filePath);
+        var data = JObject.Parse(json);
+
+        await githubClient.Issue.Comment.Create(repositoryId, prId, data["Comments"].ToString());
+    }
+}
--- tools/PullRequestReviewer/Program.cs
+++ tools/PullRequestReviewer/Program.cs
@@ -1,9 +1,24 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text.Json;
 using LibGit2Sharp;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.AI.ChatCompletion;
 using static Microsoft.SemanticKernel.AI.ChatCompletion.ChatHistory;
 
+/// <summary>
+/// 処理の結果
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// レビューのコメント
+    /// </summary>
+    /// <value></value>
+    public string Comments { get; set; }
+}
+
+/// <summary>
+/// 変更が加わったコードをGPTに投げてレビューしてもらうツール
+/// </summary>
 public static class Program
 {
     /// <summary>
@@ -14,62 +29,72 @@ public static class Program
     public static async Task Main(string[] args)
     {
         if (args.Length != 2)
-            throw new ArgumentException("引数が無効です");
+        {
+            throw new ArgumentException("引数が無効です。2つの引数が必要です: baseBranchName, headBranchName");
+        }
 
-        Console.WriteLine($"HEAD branch name: {args[0]}");
-        Console.WriteLine($"Base branch name: {args[1]}");
+        Console.WriteLine($"Base branch name: {args[0]}");
+        Console.WriteLine($"HEAD branch name: {args[1]}");
 
-        var review = await CreateReviewAsync(args[0], args[1]);
-        File.WriteAllText("review.env", $"{review}");
+        var reviewComments = await CreateReviewCommentsAsync(args[0], args[1]);
+        Console.WriteLine($"# Result\n---\n{reviewComments}");
+        File.WriteAllText("result.json", JsonSerializer.Serialize(new Result { Comments = reviewComments }, new JsonSerializerOptions { WriteIndented = true }));
     }
 
     /// <summary>
     /// レビューを作成する
     /// </summary>
-    /// <param name="headBranchName"></param>
     /// <param name="baseBranchName"></param>
+    /// <param name="headBranchName"></param>
     /// <returns></returns>
-    private static async Task<string> CreateReviewAsync(string headBranchName, string baseBranchName)
+    private static async Task<string> CreateReviewCommentsAsync(string baseBranchName, string headBranchName)
     {
-        var repoPath = Repository.Discover(Path.GetFullPath(Directory.GetCurrentDirectory()));
-
         var kernel = new KernelBuilder().Configure(c =>
         {
             c.AddOpenAIChatCompletionService("chat", "gpt-3.5-turbo", Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
         }).Build();
 
         var chatCompletionService = kernel.GetService<IChatCompletion>();
-        var systemPrompt = $"Provide some feedback and suggestions for improvement to source codes from user. Answer in Japanese.";
+        var systemPrompt = File.ReadAllText("tools/PullRequestReviewer/prompt.txt");
 
+        // リポジトリのパスを取得
+        var repoPath = Repository.Discover(Path.GetFullPath(Directory.GetCurrentDirectory()));
+
+        // Gitリポジトリを操作して、指定されたブランチ間の差分を取得
         using (var repo = new Repository(repoPath))
         {
-            var latestCommit = repo.Branches[headBranchName].Tip;
             var baseCommit = repo.Branches[baseBranchName].Tip;
+            var latestCommit = repo.Branches[headBranchName].Tip;
 
             var diff = repo.Diff.Compare<TreeChanges>(baseCommit.Tree, latestCommit.Tree);
 
-            var addedFiles = diff.Where(e => e.Status == ChangeKind.Added || e.Status == ChangeKind.Modified).Select(e => e.Path).ToList();
-
-            var addedCSharpFiles = addedFiles.Where(e => e.EndsWith(".cs")).ToList();
-
-            var classPattern = @"\bclass\s+(\w+)";
-            var interfacePattern = @"\binterface\s+(\w+)";
+            // 変更されたファイルのパスを取り出す
+            var changedFilePaths = diff.Where(e => e.Status == ChangeKind.Added || e.Status == ChangeKind.Modified).Select(e => e.Path).ToList();
 
             var responses = new List<string>();
-            foreach (var file in addedCSharpFiles)
+            foreach (var changedFilePath in changedFilePaths)
             {
-                var content = File.ReadAllText(file);
-                if (Regex.IsMatch(content, classPattern) || Regex.IsMatch(content, interfacePattern))
-                {
-                    var chat = chatCompletionService.CreateNewChat();
-                    chat.AddMessage(AuthorRoles.System, systemPrompt);
-                    chat.AddMessage(AuthorRoles.User, content);
-                    var response = await chatCompletionService.GenerateMessageAsync(chat, new ChatRequestSettings { MaxTokens = 3000 });
-                    responses.Add(response);
-                }
+                // 対象ファイルの内容取得
+                var content = File.ReadAllText(changedFilePath);
+
+                // 差分取得
+                var patch = repo.Diff.Compare<Patch>(baseCommit.Tree, latestCommit.Tree, new List<string> { changedFilePath });
+                var patchContent = patch.Content;
+
+                // GPTへリクエスト
+                var message = $"# Name\n{changedFilePath}\n" + $"# Content\n{content}\n" + $"# Diff\n{patchContent}";
+                var chat = chatCompletionService.CreateNewChat();
+                chat.AddMessage(AuthorRoles.System, systemPrompt);
+                chat.AddMessage(AuthorRoles.User, message);
+
+                Console.WriteLine($"# {changedFilePath}");
+                Console.WriteLine("Now Reviewing...");
+                var response = await chatCompletionService.GenerateMessageAsync(chat, new ChatRequestSettings { MaxTokens = 800 });
+                Console.WriteLine("Done!");
+                responses.Add(response);
             }
 
-            return string.Join("\n", responses);
+            return string.Join("\n\n", responses);
         }
     }
 }
