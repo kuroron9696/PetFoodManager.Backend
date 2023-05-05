dfb303c14068404202446e32fffb048fcd8d07dc
kuro <40154200+kuroron9696@users.noreply.github.com>
2023-05-06 05:14:17 +0900

    ⚡️ 様々な改善

--- tools/PullRequestReviewer/Program.cs
+++ tools/PullRequestReviewer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Collections.Concurrent;
+using System.Text.Json;
 using LibGit2Sharp;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
@@ -13,6 +14,7 @@ namespace PullRequestReviewer
     public class Program
     {
         private static readonly ILogger<Program> s_logger;
+        private static readonly Random s_random;
         private static readonly IKernel s_kernel;
         private static readonly IChatCompletion s_chatCompletionService;
         private static readonly int s_maxTokens;
@@ -27,17 +29,21 @@ namespace PullRequestReviewer
             var modelName = Environment.GetEnvironmentVariable("MODEL_NAME");
             var baseUrl = Environment.GetEnvironmentVariable("BASE_URL");
             var maxTokensExists = int.TryParse(Environment.GetEnvironmentVariable("MAX_TOKENS"), out s_maxTokens);
-            var useAzureOpenAIExists = bool.TryParse(Environment.GetEnvironmentVariable("USE_AZURE_OPENAI"), out var useAzureOpenAI);
+            var useAzureExists = bool.TryParse(Environment.GetEnvironmentVariable("USE_AZURE"), out var useAzure);
 
-            if (string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(modelName) || !maxTokensExists || !useAzureOpenAIExists)
+            if (string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(modelName) || !maxTokensExists || !useAzureExists)
                 throw new InvalidOperationException("環境変数が不足しています。");
 
-            if (useAzureOpenAI && string.IsNullOrEmpty(baseUrl))
-                throw new InvalidOperationException("Azure OpenAIを使用する場合はBASE_URLが必要です。");
+            if (useAzure && string.IsNullOrEmpty(baseUrl))
+                throw new InvalidOperationException("Azure OpenAI Serviceを使用する場合はBASE_URLが必要です。");
 
+            s_logger = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+            }).CreateLogger<Program>();
             s_kernel = new KernelBuilder().Configure(c =>
             {
-                if (useAzureOpenAI)
+                if (useAzure)
                 {
                     c.AddAzureChatCompletionService(s_serviceId, modelName, baseUrl, apiKey);
                 }
@@ -47,10 +53,8 @@ namespace PullRequestReviewer
                 }
             }).Build();
             s_chatCompletionService = s_kernel.GetService<IChatCompletion>();
-            s_logger = LoggerFactory.Create(builder =>
-            {
-                builder.AddConsole();
-            }).CreateLogger<Program>();
+            var seed = Environment.TickCount;
+            s_random = new ThreadLocal<Random>(() => new Random(Interlocked.Increment(ref seed))).Value;
         }
 
         /// <summary>
@@ -65,8 +69,7 @@ namespace PullRequestReviewer
                 throw new ArgumentException("引数が無効です。2つの引数が必要です: BaseBranchName, SourceBranchName");
             }
 
-            s_logger.LogInformation($"Base branch name: {args[0]}");
-            s_logger.LogInformation($"Source branch name: {args[1]}");
+            s_logger.LogInformation($"Base branch name: {args[0]} / Source branch name: {args[1]}");
 
             await CreateReviewCommentsAsync(args[0], args[1]);
         }
@@ -80,7 +83,7 @@ namespace PullRequestReviewer
         private static async Task CreateReviewCommentsAsync(string baseBranchName, string sourceBranchName)
         {
             // レビュー用プロンプトを取得
-            var systemPrompt = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "prompt.txt"));
+            var systemPrompt = File.ReadAllText("tools/PullRequestReviewer/prompt.txt");
 
             // リポジトリのパスを取得
             var repoPath = Repository.Discover(Path.GetFullPath(Directory.GetCurrentDirectory()));
@@ -96,33 +99,52 @@ namespace PullRequestReviewer
                 // 変更されたファイルのパスを取り出す
                 var changedFilePaths = diff.Where(e => e.Status == ChangeKind.Added || e.Status == ChangeKind.Modified).Select(e => e.Path).ToList();
 
-                var reviewComments = new List<string>();
-                foreach (var changedFilePath in changedFilePaths)
+                var reviewComments = new ConcurrentBag<string>();
+                var tasks = new ConcurrentBag<Task>();
+
+                Parallel.ForEach(changedFilePaths, changedFilePath =>
                 {
-                    // 対象ファイルの内容取得
-                    var content = File.ReadAllText(changedFilePath);
+                    tasks.Add(Task.Run(async () =>
+                    {
+                        // APIのレートリミット対策
+                        var waitingSeconds = s_random.Next(0, 180);
+                        s_logger.LogInformation($"Wait for {waitingSeconds} seconds to avoid rate limits. / Subject: {changedFilePath}");
+                        await Task.Delay(waitingSeconds * 1000);
 
-                    // 差分取得
-                    var patch = repo.Diff.Compare<Patch>(baseCommit.Tree, latestCommit.Tree, new List<string> { changedFilePath });
-                    var patchContent = patch.Content;
+                        s_logger.LogInformation($"Now Reviewing... / Subject: {changedFilePath}");
 
-                    // GPTへリクエスト
-                    var message = $"# Name\n{changedFilePath}\n" + $"# Content\n{content}\n" + $"# Diff\n{patchContent}";
+                        // 対象ファイルの内容取得
+                        var content = File.ReadAllText(changedFilePath);
 
-                    var chat = s_chatCompletionService.CreateNewChat();
-                    chat.AddMessage(AuthorRoles.System, systemPrompt);
-                    chat.AddMessage(AuthorRoles.User, message);
+                        // 差分取得
+                        var patch = repo.Diff.Compare<Patch>(baseCommit.Tree, latestCommit.Tree, new List<string> { changedFilePath });
+                        var patchContent = patch.Content;
 
-                    s_logger.LogInformation($"# {changedFilePath}");
-                    s_logger.LogInformation("Now Reviewing...");
+                        // GPTへリクエスト
+                        var message = $"# Name\n{changedFilePath}\n" + $"# Content\n{content}\n" + $"# Diff\n{patchContent}";
 
-                    var reviewComment = await s_chatCompletionService.GenerateMessageAsync(chat, new ChatRequestSettings { MaxTokens = s_maxTokens });
+                        var chat = s_chatCompletionService.CreateNewChat();
+                        chat.AddMessage(AuthorRoles.System, systemPrompt);
+                        chat.AddMessage(AuthorRoles.User, message);
 
-                    s_logger.LogInformation("Done!");
+                        var reviewComment = $"## {changedFilePath}\n";
+                        try
+                        {
+                            reviewComment = await s_chatCompletionService.GenerateMessageAsync(chat, new ChatRequestSettings { MaxTokens = s_maxTokens });
+                        }
+                        catch (Exception ex)
+                        {
+                            s_logger.LogError($"An error occured. (Message: {ex.Message}) / Subject: {changedFilePath}");
+                            reviewComment += "エラーが発生したため、レビューできませんでした。";
+                        }
 
-                    reviewComments.Add(reviewComment);
-                }
+                        reviewComments.Add(reviewComment);
 
+                        s_logger.LogInformation($"Done! / Subject: {changedFilePath}");
+                    }));
+                });
+
+                await Task.WhenAll(tasks);
                 File.WriteAllText("result.json", JsonSerializer.Serialize(new Result { Comment = string.Join("\n\n", reviewComments) }, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true }));
             }
         }
